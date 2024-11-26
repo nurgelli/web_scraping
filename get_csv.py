216bd26e3ff6103ae14e9ccf42ecd0f1ed598c17
nurgelli <nt.interpretor@gmail.com>
2024-11-26 19:49:43 +0500

    seperated files

--- get_csv.py
+++ get_csv.py
@@ -2,10 +2,7 @@ import csv
 
 def save_csv(results):
     keys = results[0].keys()
-    try:
-        with open('products_info.csv', 'w', encoding='utf-8') as file:
-            product_writer = csv.DictWriter(file, keys)
-            product_writer.writeheader()
-            product_writer.writerows(results)
-    except NotImplementedError as err:
-        print(f"Can't be written {err}")
+    with open('products_info.csv', 'w', encoding='utf-8') as file:
+        product_writer = csv.DictWriter(file, keys)
+        product_writer.writeheader()
+        product_writer.writerows(results)
